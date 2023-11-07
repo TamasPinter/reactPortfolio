53c7980a06302fa5d516fb95a0479d3eb2829ce1
Tomas P <brolli_673@hotmail.com>
2023-11-07 09:59:11 -0500

    updated portfolio

--- src/components/projects.js
+++ src/components/projects.js
@@ -6,67 +6,73 @@ import SocialImg from "../Assets/Nano.gif";
 import CodingImg from "../Assets/coding.gif";
 import ArcadeImg from "../Assets/Arcade.gif";
 
-
 function Projects() {
   const data = [
+    {
+      title: "Pearly Whites Dental Hygiene",
+      imageLink: TextImg,
+      activeLink: "https://tamaspinter.github.io/Dental/",
+      repoLink: "https://github.com/TamasPinter/Dental.git",
+    },
     {
       title: "Cafe Du Lait",
       imageLink: SocialImg,
       activeLink: "https://cafe-au-lait.herokuapp.com/",
-      repoLink: "https://github.com/porteous89/cafe-au-lait"
+      repoLink: "https://github.com/porteous89/cafe-au-lait",
     },
     {
-      title: "Freshest Tomatoes",
+      title: "Command Line Chain level Store Database",
       imageLink: TomatoesImg,
+      activeLink: "https://github.com/TamasPinter/CLChainStoreDb",
+      repoLink: "https://github.com/TamasPinter/CLChainStoreDb.git",
+    },
+    {
+      title: "JavaScript Calculator",
+      imageLink: ArcadeImg,
+      activeLink: "https://tamaspinter.github.io/ToPC/",
+      repoLink: "https://github.com/TamasPinter/ToPC.git",
+    },
+    {
+      title: "The Freshest Tomatoes",
+      imageLink: BudgetGameImg,
       activeLink: "https://project-2-fresh-tomatoes.herokuapp.com/",
       repoLink: "https://github.com/porteous89/project-2-fresh-tomatoes",
     },
-    {
-      title: "Best Game For Your Buck",
-      imageLink: BudgetGameImg,
-      activeLink: "https://mitsushiro1.github.io/The-best-game-for-your-buck/",
-      repoLink: "https://github.com/mitsushiro1/The-best-game-for-your-buck",
-    },
-    {
-      title: "PWA Text Editor",
-      imageLink: TextImg,
-      activeLink: "https://mysterious-stream-67239.herokuapp.com/",
-      repoLink: "https://github.com/TamasPinter/Text_EditorPWA",
-    },
+
     {
       title: "Employee Database Backend",
       imageLink: CodingImg,
       activeLink: "https://github.com/TamasPinter/employeeTracker",
       repoLink: "https://github.com/TamasPinter/employeeTracker",
     },
-    {
-      title: "Mini Game Page",
-      imageLink: ArcadeImg,
-      activeLink: "https://tamaspinter.github.io/smallGame/",
-      repoLink: "https://github.com/TamasPinter/smallGame",
-    },
-    
   ];
   return (
-    <div className="projects" class="p-5 mb-4 bg-light container-fluid py-5 row">
+    <div
+      className="projects"
+      class="p-5 mb-4 bg-light container-fluid py-5 row"
+    >
       <h1 class="text-center">Projects</h1>
       {data.map((e) => (
         <div class="col-md-4">
-        <div class="card text-white bg-dark mb-3 boxes">
-          <h3 class="text-center">{e.title}</h3>
-          <img src={e.imageLink} alt="projectImg" height="250" width="345" class="rounded mx-auto d-block"/><br/>
-          <div class="card-body text-center">
-          <a href={e.activeLink}>Live Application</a>
-          <a href={e.repoLink}>Github Repository</a>
+          <div class="card text-white bg-dark mb-3 boxes">
+            <h3 class="text-center">{e.title}</h3>
+            <img
+              src={e.imageLink}
+              alt="projectImg"
+              height="250"
+              width="345"
+              class="rounded mx-auto d-block"
+            />
+            <br />
+            <div class="card-body text-center">
+              <a href={e.activeLink}>Live Application</a>
+              <a href={e.repoLink}>Github Repository</a>
+            </div>
+          </div>
         </div>
-        </div>
-        </div>
-        
-        
       ))}
     </div>
   );
 }
-
 
 export default Projects;
--- src/components/resume.js
+++ src/components/resume.js
@@ -1,7 +1,6 @@
 import React from "react";
-import MyResume from "../Assets/Resume.pdf";
+import MyResume from "../Assets/ResumePDF.pdf";
 import Matrix from "../Assets/matrix.jpg";
-
 
 function Resume() {
   return (
@@ -9,36 +8,69 @@ function Resume() {
       <div class="card">
         <h1 class="text-center">My Proficiencies</h1>
         <div class="rows-col-3" style={{ backgroundImage: `url(${Matrix})` }}>
-          <div class="card-body boxes col-md-3">
+          <div class="proficiencies">
+            <div class="card-body boxes col-md-3">
               <button type="button" class="btn btn-primary">
                 HTML<span class="badge bg-secondary"></span>
-          </button>
-          </div>
-          <div class="card-body boxes col-3">
+              </button>
+            </div>
+            <div class="card-body boxes col-3">
               <button type="button" class="btn btn-primary">
                 CSS<span class="badge bg-success"></span>
-              </button></div>
-          <div class="card-body boxes col-3">
+              </button>
+            </div>
+            <div class="card-body boxes col-3">
               <button type="button" class="btn btn-primary">
                 JavaScript<span class="badge bg-secondary"></span>
-                </button></div>
-          <div class="card-body boxes col-3">
+              </button>
+            </div>
+            <div class="card-body boxes col-3">
               <button type="button" class="btn btn-primary">
                 JQUERY<span class="badge bg-secondary"></span>
-              </button></div>
-          <div class="card-body boxes col-3">
+              </button>
+            </div>
+            <div class="card-body boxes col-3">
               <button type="button" class="btn btn-primary">
                 Node/NodeJS<span class="badge bg-secondary"></span>
-              </button></div>
-          <div class="card-body boxes col-3">
+              </button>
+            </div>
+            <div class="card-body boxes col-3">
               <button type="button" class="btn btn-primary">
                 MongoDb<span class="badge bg-secondary"></span>
-              </button></div>
-          <div class="card-body boxes col-3">
+              </button>
+            </div>
+            <div class="card-body boxes col-3">
               <button type="button" class="btn btn-primary">
-              React/Mern<span class="badge bg-secondary"></span>
-              </button></div>
-       </div>
+                React/Mern<span class="badge bg-secondary"></span>
+              </button>
+            </div>
+            <div class="card-body boxes col-3">
+              <button type="button" class="btn btn-primary">
+                Front-End<span class="badge bg-secondary"></span>
+              </button>
+            </div>
+            <div class="card-body boxes col-3">
+              <button type="button" class="btn btn-primary">
+                Back-End<span class="badge bg-secondary"></span>
+              </button>
+            </div>
+            <div class="card-body boxes col-3">
+              <button type="button" class="btn btn-primary">
+                Canvas<span class="badge bg-secondary"></span>
+              </button>
+            </div>
+            <div class="card-body boxes col-3">
+              <button type="button" class="btn btn-primary">
+                Responsivity<span class="badge bg-secondary"></span>
+              </button>
+            </div>
+            <div class="card-body boxes col-3">
+              <button type="button" class="btn btn-primary">
+                Git-Workflow<span class="badge bg-secondary"></span>
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
       <div className="resumeText" class="text-center">
         <a href={MyResume}>My Resume</a>
